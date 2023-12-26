d70952cfc79544ca1ccd4ff86e522246d36424ce
Diego da Mata <Diegodamata>
2023-12-26 17:48:34 -0300

    Primeira classe: Posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
+import tabuleiro_jogo.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello world!!!");
+		Posicao posi = new Posicao(3, 4);
 
+		
+		System.out.println(posi);
 	}
 
 }
